9be114606cc2d2293741eb8afcecb96e0719527a
Daniel Escobar <escobard90@gmail.com>
2016-11-19 15:37:14 -0700

    type: feat
    added browser sync trackers to gulp.js

--- application/gulpfile.js
+++ application/gulpfile.js
@@ -18,6 +18,9 @@ gulp.task('default', ['copy-html', 'copy-images', 'styles', 'lint', 'scripts'], 
 	gulp.watch('components/signup.html').on('change', browserSync.reload);
 	gulp.watch('components/main-app.html').on('change', browserSync.reload);
 	gulp.watch('components/event-planner.html').on('change', browserSync.reload);
+	gulp.watch('components/event-login.html').on('change', browserSync.reload);	
+	gulp.watch('components/event-login-form.html').on('change', browserSync.reload);	
+	gulp.watch('components/event-registration-form.html').on('change', browserSync.reload);
 	gulp.watch('components/event-planner-editor.html').on('change', browserSync.reload);
 });
 
